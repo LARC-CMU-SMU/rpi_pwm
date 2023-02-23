c5adce44500d1d6ce814198bc7ef18dac6848334
kasun <kasun.com>
2023-02-23 14:23:59 +0800

    updated

--- hw_pwm_const.py
+++ hw_pwm_const.py
@@ -4,7 +4,7 @@ import time
 pin = 18
 start_sleep_time = 2
 freq = 300 # this is the lowest freq blinking is invisible to slo mo video
-dc = .005 # range ~0.004-100 0 = off, 100 = fully on
+dc = 10 # range ~0.004-100 0 = off, 100 = fully on
 dc = dc * 10000 # real range is 0 - 1000000
 
 pi=pigpio.pi()
